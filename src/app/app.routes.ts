8049d9399bd658669edec30d61791abb1ec362f3
estertzefira <ester100499@gmail.com>
2025-07-30 15:09:43 +0300

    SCRUM-4 | 404 page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import {HomePage} from './components/home-page/home-page';
 import {FullWidthLayout} from './components/layouts/full-width-layout/full-width-layout';
 import {ContainerLayout} from './components/layouts/container-layout/container-layout';
 import {NewEvent} from './components/new-event/new-event';
+import {PageNotFound} from './components/page-not-found/page-not-found';
 
 export const routes: Routes = [
   {
@@ -12,6 +13,10 @@ export const routes: Routes = [
       {
         path: '',
         component: HomePage
+      },
+      {
+        path: '**',
+        component: PageNotFound
       }
     ]
   },
